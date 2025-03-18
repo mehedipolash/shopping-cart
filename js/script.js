04d5010f18a63b2520a2c9089a2a4db0f6cdb6a6
mehedipolash <mehedipolash228@gmail.com>
2025-03-18 17:37:41 +0600

    size btn done

--- js/script.js
+++ js/script.js
@@ -1,22 +1,55 @@
 const ringButtons = document.querySelectorAll('.ring-button');
 
+
+// button border added and main page image change related simple code
 for (let i = 0; i < ringButtons.length; i++) {
+  
   const ringBtn = ringButtons[i];
-  // console.log(ringButtons[i]);
+  
 
   ringBtn.addEventListener('click', function (event) {
-    //color add korar age sobgulake check kore jodi dekhi purple ase tahole cpolor remove kore dibo ebong gray add kore dibo
+    
+    const color = event.target.id.replace('-color','')
+    console.log(color);
 
-    for (let j = 0; j < ringButtons.length; j++) {
-      ringButtons[j].classList.remove('border-red-500');
-      ringButtons[j].classList.add('border-gray-500');
-    }
+     for (let j = 0; j < ringButtons.length; j++) {
+       ringButtons[j].classList.remove('border-red-500');
+       
+     }
 
-    //color add kortesi
+ 
     event.target.classList.add('border-red-500');
-    event.target.classList.remove('border-gray-500');
+
+    //(since this is the latest line so red color will activated when clicking)
+
 
     const productImage = document.getElementById('product-image');
-    // productImage.src = '../images/teal.png';
+    
+    
+
+    //dynamically add color
+    productImage.src = "../images/" + color + ".png";
   });
+}
+
+
+//wrist size related code (main html has onclick for buttons to make code easy)
+function selectWristSize(size) {
+  
+  //console.log(size)
+  const sizes = ['M', 'L', 'S', 'XL']
+
+  for (object of sizes) {
+    const element = object;
+
+    const button = document.getElementById('size-' + element);
+
+    if (size === element) {
+      button.classList.add('border-purple-600');
+    }
+    else {
+      button.classList.remove('border-purple-600');
+    }
+    
+  }
 }
